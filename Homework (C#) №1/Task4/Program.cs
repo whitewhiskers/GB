e0b83952f04e5d0af411edd7292ecb007903ed24
Mikail Belousov <whitewhiskers96@gmail.com>
2022-08-09 20:10:01 +0300

    add task4 in homework C#

---
+++ Homework (C#) №1/Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход 
+// три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+System.Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите третье число: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+if (number2>max) max = number2;
+if (number3>max) max = number3;
+System.Console.Write($"максимальное число: {max}");
