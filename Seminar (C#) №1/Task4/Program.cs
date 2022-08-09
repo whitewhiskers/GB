b55a357258e23d57f6606f74aa6133cc76b98baa
Mikail Belousov <whitewhiskers96@gmail.com>
2022-08-09 19:29:20 +0300

    remove marks(task4)

--- Seminar (C#) №1/Task4/Program.cs
+++ Seminar (C#) №1/Task4/Program.cs
@@ -1,4 +1,4 @@
 ﻿System.Console.Write("Введите трехзначное число: ");
 int numbers = Convert.ToInt32(Console.ReadLine());
 numbers = numbers % 10;
-System.Console.WriteLine($"Ваше посленее число трехзначного числа = {numbers}");
+System.Console.WriteLine($"Ваше последнее число трехзначного числа = {numbers}");
