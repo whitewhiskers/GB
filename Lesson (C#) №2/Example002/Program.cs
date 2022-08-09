13034bc3201c5cb18bbef174546d613d27376caa
Mikail Belousov <whitewhiskers96@gmail.com>
2022-08-09 21:55:50 +0300

    add lesson 2 c#

---
+++ Lesson (C#) №2/Example002/Program.cs
@@ -0,0 +1,31 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int[] array = { 18, 12, 33, 24, 555, 56, 77, 28, 9 };
+
+int max = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
+System.Console.WriteLine(max);
+
+
+
+
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max(max1, max2, max3);
+
+// int max = Max(
+//     Max(a1, b1, c1), 
+//     Max(a2, b2, c2), 
+//     Max(a3, b3, c3));
+
+// System.Console.WriteLine(max);
---
+++ Lesson (C#) №2/Example003/Program.cs
@@ -0,0 +1,14 @@
+﻿int [] array = {15,21,38,42,48,75,66,77,48};
+
+int n = array.Length;
+int find = 48;
+int index = 0;
+while (index<n)
+{
+    if (array[index] == find)
+    {
+        System.Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
