25c0eabffd848ccbd6c0bed3826bc92ec9c84879
Mikail Belousov <whitewhiskers96@gmail.com>
2022-08-09 18:45:04 +0300

    add tasks 1-2

---
+++ Seminar (C#) №1/Taks1/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 == number2 * number2)
+{
+    System.Console.WriteLine($"{number1} является квардратом {number2}");
+}
+else
+{
+    System.Console.WriteLine($"{number1} не является квардратом {number2}");
+}
---
+++ Seminar (C#) №1/Task2/Program.cs
@@ -0,0 +1,9 @@
+﻿System.Console.Write("Введите номер для недели: ");
+byte number = Convert.ToByte(Console.ReadLine());
+if (number == 1) Console.Write("Сегодня понедельник!");
+if (number == 2) Console.Write("Сегодня вторник!");
+if (number == 3) Console.Write("Сегодня среда!");
+if (number == 4) Console.Write("Сегодня четверг!");
+if (number == 5) Console.Write("Сегодня пятница!");
+if (number == 6) Console.Write("Сегодня суббота!");
+if (number == 7) Console.Write("Сегодня воскресенье!");
