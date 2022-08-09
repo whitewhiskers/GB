13ece9faaa07164f100809f1c83c402bedf73cf0
Mikail Belousov <whitewhiskers96@gmail.com>
2022-08-09 20:36:49 +0300

    Format text in Task4

--- Homework (C#) №1/Task4/Program.cs
+++ Homework (C#) №1/Task4/Program.cs
@@ -13,4 +13,4 @@ int number3 = Convert.ToInt32(Console.ReadLine());
 int max = number1;
 if (number2>max) max = number2;
 if (number3>max) max = number3;
-System.Console.Write($"максимальное число: {max}");
+System.Console.Write($"Максимальное число: {max}");
