918cfacd43c4160fd55ee6fa318f0a500e500b5a
Mikail Belousov <whitewhiskers96@gmail.com>
2022-08-09 16:05:24 +0300

    add to lower

---
+++ Example005_If_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "миша")
+{
+    Console.WriteLine("Ура это Миша! Привет!");
+}
+else
+{
+    Console.Write("Привет! ");
+    Console.WriteLine(username);
+}
