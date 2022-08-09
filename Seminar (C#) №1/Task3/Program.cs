9f92d7182a352a148a01691fff2e9c8a39e11ea3
Mikail Belousov <whitewhiskers96@gmail.com>
2022-08-09 19:28:03 +0300

    add tasks 3-4

---
+++ Seminar (C#) №1/Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.Write("Введите ваше число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int numberMinus = -number;
+while (numberMinus<number)
+{
+    
+    Console.Write($"{numberMinus},");
+    numberMinus++;
+}
+if(numberMinus == number)
+    {
+        Console.Write(numberMinus);
+        numberMinus++;
+    }
---
+++ Seminar (C#) №1/Task4/Program.cs
@@ -0,0 +1,4 @@
+﻿System.Console.Write("Введите трехзначное число: ");
+int numbers = Convert.ToInt32(Console.ReadLine());
+numbers = numbers % 10;
+System.Console.WriteLine($"Ваше посленее число трехзначного числа = {numbers}");
