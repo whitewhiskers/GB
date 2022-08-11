633f970e61997db75361161d9c56cdf5b3afef22
Mikail Belousov <whitewhiskers96@gmail.com>
2022-08-11 13:55:35 +0300

    add task 1

--- Seminar (C#) №2/task0/Program.cs
+++ Seminar (C#) №2/task0/Program.cs
@@ -7,10 +7,19 @@
 
 int number = new Random().Next(10, 100);
 System.Console.WriteLine($"Ваше случайное число из отрезка [10,99] -> {number}");
-int a = number / 10;
-int b = number % 10;
-if (a > b) System.Console.WriteLine($"Наибольшая цифра числа {number} -> {a}");
-else if (a < b) System.Console.WriteLine($"Наибольшая цифра числа {number} -> {b}");
-else System.Console.WriteLine("Цифры равны");
+// int a = number / 10;
+// int b = number % 10;
+// if (a > b) System.Console.WriteLine($"Наибольшая цифра числа {number} -> {a}");
+// else if (a < b) System.Console.WriteLine($"Наибольшая цифра числа {number} -> {b}");
+// else System.Console.WriteLine("Цифры равны");
 
+int MaxChar(int num)
+{
+    int a = num / 10;
+    int b = num % 10;
+    if (a > b) return a;
+    return b;
+}
 
+int result = MaxChar(number);
+System.Console.WriteLine($"Наибольшая цифра числа {number} -> {result}");
---
+++ Seminar (C#) №2/task1/Program.cs
@@ -0,0 +1,12 @@
+﻿int number = new Random().Next(100,1000);
+System.Console.WriteLine($"Ваше случайное трехзначное число: {number}");
+
+int DeleteMiddleChar(int num)
+{
+    int firstchar = num / 100;
+    int lastchar = num % 10;
+    return firstchar*10+lastchar;
+}
+
+int result = DeleteMiddleChar(number);
+System.Console.WriteLine($"Ваше число без средней цифры: {result}");
