9c45edf5d08f8cdf60f033d4a27a4eaebada48e0
Mikail Belousov <whitewhiskers96@gmail.com>
2022-08-11 14:51:01 +0300

    add task2

---
+++ Seminar (C#) №2/task2/Program.cs
@@ -0,0 +1,9 @@
+﻿System.Console.WriteLine("Введите ваше число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 7 == 0 && number % 23 == 0)
+{
+    System.Console.WriteLine($"Ваше число {number} делится на 7 и на 23");
+}
+else System.Console.WriteLine($"Ваше число {number} не делится на 7 и на 23");
+
+
