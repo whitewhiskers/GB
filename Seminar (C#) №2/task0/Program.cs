eb7456eba620110fa55d0d0cbbe29013a0dc9f82
Mikail Belousov <whitewhiskers96@gmail.com>
2022-08-11 12:33:57 +0300

    add seminar2 and task 0

---
+++ Seminar (C#) №2/task0/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая 
+// выводит случайное число из отрезка [10,99]
+// и показывает наибольшую цифру числа.
+// 78 -> 8
+// 56 -> 6 
+// 95 -> 9
+
+int number = new Random().Next(10, 100);
+System.Console.WriteLine($"Ваше случайное число из отрезка [10,99] -> {number}");
+int a = number / 10;
+int b = number % 10;
+if (a > b) System.Console.WriteLine($"Наибольшая цифра числа {number} -> {a}");
+else if (a < b) System.Console.WriteLine($"Наибольшая цифра числа {number} -> {b}");
+else System.Console.WriteLine("Цифры равны");
+
+
