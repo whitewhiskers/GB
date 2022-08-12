24ccdf7da869cbf2f6a7d3e1736ffd2a6b80b1be
Mikhail <whitewhiskers96@gmail.com>
2022-08-12 21:58:05 +0300

    add task 23 in homework

---
+++ Homework/Homework (C#) №3/task23/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+System.Console.Write("Введите ваше число:");
+int number = int.Parse(Console.ReadLine());
+void CalcTable(int num)
+{
+    if (num > 0)
+    {
+        for (int i = 1; i <= num; i++)
+        {
+            System.Console.WriteLine($"{i}\t{i * i * i}");
+        }
+    }
+    else if (num < 0)
+    {
+        for (int i = 0; i >= num; i--)
+        {
+            System.Console.WriteLine($"{i}\t{i * i * i}");
+        }
+    }
+    else System.Console.WriteLine("Введено неверное число");
+}
+System.Console.WriteLine("Ваша таблица чисел в кубе:");
+CalcTable(number);
