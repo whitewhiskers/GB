69da042b4d56e03f3f343195a0026c9ab1535c7a
Mikhail <whitewhiskers96@gmail.com>
2022-08-12 16:51:20 +0300

    add task10

---
+++ Homework (C#) №2/task10/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает 
+// вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+System.Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+if (number < 1000 && number > 99)
+{
+    result = number / 10 % 10;
+    System.Console.WriteLine($"Ваша средняя цифра числа {number} -> {result}");
+}
+else System.Console.WriteLine($"Вы ввели не трехзначное число");
