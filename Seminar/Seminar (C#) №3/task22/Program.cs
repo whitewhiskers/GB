c4ad7f4ee419616b569d3566f1784680ff3e5a63
Mikhail <whitewhiskers96@gmail.com>
2022-08-12 21:49:47 +0300

    add task22

---
+++ Seminar/Seminar (C#) №3/task22/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+System.Console.Write("Введите ваше число:");
+int number = int.Parse(Console.ReadLine());
+void CalcTable(int num)
+{
+    if (num>0)
+    {
+        for (int i = 1; i<=num; i++)
+        {
+            System.Console.WriteLine($"{i}\t{i*i}");
+        }
+    }
+    else if (num<0)
+    {
+        for (int i = 0; i>=num; i--)
+        {
+            System.Console.WriteLine($"{i}\t{i*i}");
+        }
+    }
+    else System.Console.WriteLine("Введено неверное число");
+}
+System.Console.WriteLine("Ваша таблица:");
+CalcTable(number);
