768fe949103308c8e2149db8369e30173b54f501
Mikail Belousov <whitewhiskers96@gmail.com>
2022-08-16 12:35:10 +0300

    add task25

---
+++ Homework/Homework (C#) №4/Task25/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 25: Напишите цикл, который принимает на 
+// вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+System.Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int Exponentiation(int num1, int num2)
+{
+    int result = 1;
+    for(int i = 0; i < num2; i++)
+    {
+     result = result * num1;
+    }
+    return result;
+}
+
+int exponent = Exponentiation(number1,number2);
+System.Console.WriteLine($"Ваше число {number1} в степени {number2} => {exponent}");
