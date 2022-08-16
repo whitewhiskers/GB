476df010f658e0ffbc702e2c29541afa3ebf275c
Mikail Belousov <whitewhiskers96@gmail.com>
2022-08-16 12:47:04 +0300

    add task27

---
+++ Homework/Homework (C#) №4/Task27/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+System.Console.Write("Введите ваше число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumDigits (int num)
+{
+    int res = 0;
+    num = Math.Abs(num); 
+    while(num > 0)
+    {
+        res = res + (num % 10);
+        num = num /10;
+    }
+    return res;
+}
+
+int sum = SumDigits(number);
+System.Console.WriteLine($"Сумма чисел в вашем числе {number} -> {sum}");
