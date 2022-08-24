9ae4d144236c4e72bed3b4e0ddbe12f63c7d120c
Mikail Belousov <whitewhiskers96@gmail.com>
2022-08-24 13:50:49 +0300

    add task35,37

---
+++ Seminar/Seminar (C#) №5/task35/Program.cs
@@ -0,0 +1,34 @@
+﻿int[] FillArray(int length)
+{
+    int[] array = new int[length];
+    Random rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rnd.Next(1, 123);
+    }
+    return array;
+}
+void PrintArray(int[] arr)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) System.Console.Write($"{arr[i]},");
+        else System.Console.Write($"{arr[i]}");
+    }
+    System.Console.WriteLine("]");
+}
+
+int CheckValue(int [] arr, int min, int max)
+{
+    int numberValue = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > min-1 && arr[i] < max-1) numberValue++;
+    }
+    return numberValue;
+}
+int[] array = FillArray(123);
+PrintArray(array);
+int value = CheckValue(array,10,99);
+System.Console.WriteLine($"Количество чисел на отрезке [10,99] -> {value}");
---
+++ Seminar/Seminar (C#) №5/task37/Program.cs
@@ -0,0 +1,51 @@
+﻿int[] FillArray(int length)
+{
+    int[] array = new int[length];
+    Random rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rnd.Next(1, 10);
+    }
+    return array;
+}
+void PrintArray(int[] arr)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) System.Console.Write($"{arr[i]},");
+        else System.Console.Write($"{arr[i]}");
+    }
+    System.Console.WriteLine("]");
+}
+int[] MyltiplPairNum(int[] arr)
+{
+    if (arr.Length % 2 == 0)
+    {
+        int[] array = new int[arr.Length / 2];
+        int l = -1;
+        for (int i = 0; i < arr.Length / 2; i++)
+        {
+            array[i] = arr[i] * arr[arr.Length + l];
+            l--;
+        }
+        return array;
+    }
+    else
+    { 
+        int[] array = new int[arr.Length / 2 +1];
+        int l = -1;
+        for (int i = 0; i < arr.Length / 2; i++)
+        {
+            array[i] = arr[i] * arr[arr.Length + l];
+            l--;
+        }
+        array[array.Length-1] = arr[arr.Length / 2];
+        return array;
+    }
+}
+int[] array = FillArray(3);
+PrintArray(array);
+int[] array2 = MyltiplPairNum(array);
+PrintArray(array2);
+
