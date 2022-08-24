095f35c9e085d32ab8adf6f26fdbd6e4a2a45924
Mikail Belousov <whitewhiskers96@gmail.com>
2022-08-24 11:40:16 +0300

    add remark task31

--- Seminar/Seminar (C#) №5/task31/Program.cs
+++ Seminar/Seminar (C#) №5/task31/Program.cs
@@ -17,14 +17,14 @@ int sumMinus = 0;
 for (int i = 0; i < array.Length; i++)
 {
     if (array[i] < 0) sumMinus += array[i];
-    else if (array[i] > 0) sumPlus += array[i];
+    else sumPlus += array[i];
 }
 System.Console.Write("[");
-for (int i = 0; i < array.Length-1; i++)
+for (int i = 0; i < array.Length; i++)
 {
-    System.Console.Write($"{array[i]},");
+    if (i<array.Length-1) System.Console.Write($"{array[i]},");
+    else System.Console.Write($"{array[i]}");
 }
-System.Console.Write($"{array[array.Length-1]}]");
-System.Console.WriteLine();
+System.Console.WriteLine("]");
 System.Console.WriteLine($"Сумма положительных чисел = {sumPlus}");
 System.Console.WriteLine($"Сумма отрицательных чисел = {sumMinus}");
