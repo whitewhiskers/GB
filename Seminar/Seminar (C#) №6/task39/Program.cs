e677c6d1738eb53843418c46e66c92daa281113b
Mikail Belousov <whitewhiskers96@gmail.com>
2022-09-01 20:17:42 +0300

    add tasks ,39,40,42

---
+++ Seminar/Seminar (C#) №6/task39/Program.cs
@@ -0,0 +1,38 @@
+﻿int[] FillArray(int length)
+{
+    int[] array = new int[length];
+    Random rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rnd.Next(1, 10);
+    }
+    return array;
+}
+void PrintArray(int[] arr)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) System.Console.Write($"{arr[i]},");
+        else System.Console.Write($"{arr[i]}");
+    }
+    System.Console.Write("]");
+}
+void ReverseArray(int [] arr)
+{
+    int size = arr.Length;
+    for (int i = 0; i < size / 2; i++)
+    {
+        int tmp = arr[i];
+        arr[i] = arr[size-1-i];
+        arr[size-1-i] = tmp;
+    }
+}
+Console.Clear();
+System.Console.Write("Введите длину массива = ");
+int number = int.Parse(Console.ReadLine());
+int[] array = FillArray(number);
+PrintArray(array);
+System.Console.WriteLine();
+ReverseArray(array);
+PrintArray(array);
---
+++ Seminar/Seminar (C#) №6/task40/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+System.Console.Write("Введите первую сторону треугольника: ");
+int num1 = int.Parse(Console.ReadLine());
+System.Console.Write("Введите вторую сторону треугольника: ");
+int num2 = int.Parse(Console.ReadLine());
+System.Console.Write("Введите третью сторону треугольника: ");
+int num3 = int.Parse(Console.ReadLine());
+
+if (num1 < num2 + num3 && num2 < num1 + num3 && num3 < num2 + num1)
+{System.Console.WriteLine("Такой треугольник может быть");}
+else System.Console.WriteLine("Такого треугольника быть не может");
