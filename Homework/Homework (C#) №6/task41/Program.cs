6c1cc533224ec4e360db3bcfa64ce9ec186a8066
Mikail Belousov <whitewhiskers96@gmail.com>
2022-09-01 21:38:08 +0300

    add task41,43

---
+++ Homework/Homework (C#) №6/task41/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+System.Console.Write("Введите количество чисел которые будите вводить:  ");
+int countPos = 0;
+int count = int.Parse(Console.ReadLine());
+for (int i = 1; i <= count; i++)
+{
+    System.Console.Write($"Введите {i} число: ");
+    int number = int.Parse(Console.ReadLine());
+    if (number > 0) countPos++;
+}
+System.Console.Write($"Положительных чисел -> {countPos}");
---
+++ Homework/Homework (C#) №6/task43/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку 
+// пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+Console.Clear();
+System.Console.Write("Введите b1: ");
+double b1 = int.Parse(Console.ReadLine());
+System.Console.Write("Введите k1: ");
+double k1 = int.Parse(Console.ReadLine());
+System.Console.Write("Введите b2: ");
+double b2 = int.Parse(Console.ReadLine());
+System.Console.Write("Введите k2: ");
+double k2 = int.Parse(Console.ReadLine());
+double x = 0, y = 0;
+if (k1==k2 && b2==b1) 
+{
+    System.Console.WriteLine("Прямые совпадают");
+    return;
+}
+if (k1==k2)
+{
+    System.Console.WriteLine("Прямые параллельны");
+    return;
+}
+x = Math.Round((b2-b1)/(k1-k2),2);
+y = Math.Round(k1*(b2-b1)/(k1-k2)+b1,2);
+System.Console.Write($"Точка пересечения прямых -> ({x}; {y})");
