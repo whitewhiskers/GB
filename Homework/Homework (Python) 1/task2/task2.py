26d20f5b979c3e8ce455081d986a6ad4c27e4a01
whitewhiskers <whitewhiskers96@gmail.com>
2023-01-03 12:18:58 +0300

    rework task 2

--- Homework/Homework (Python) 1/task2/task2.py
+++ Homework/Homework (Python) 1/task2/task2.py
@@ -1,25 +1,9 @@
-# Напишите программу для. 
-# проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z 
-# для всех значений предикат.
+# Напишите программу для проверки ложности утверждения
+# (W ⋀ Z) ⋁ ¬Y ⋁ (¬X ≡ ¬W) для всех значений предикат.
 
-def inputNumbers(x):
-    xyz = ["X", "Y", "Z"]
-    a = []
-    for i in range(x):
-        a.append(input(f"Введите значение {xyz[i]}: "))
-    return a
-
-
-def checkPredicate(x):
-    left = not (x[0] or x[1] or x[2])
-    right = not x[0] and not x[1] and not x[2]
-    result = left == right
-    return result
-
-
-statement = inputNumbers(3)
-
-if checkPredicate(statement) == True:
-    print(f"Утверждение истинно")
-else:
-    print(f"Утверждение ложно")
+for w in range(2):
+        for z in range(2):
+            for y in range(2):
+                for x in range(2):
+                    if not(w and z or not y or not (x == w)):
+                        print(x,z,y,x)
